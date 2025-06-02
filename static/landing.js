a6066aaa834763d2ce4ddafd2d59ee1d3a5a40a3
Mayank <mayank.r2409@gmail.com>
2025-06-02 21:04:29 +0530

    Responsive

--- static/landing.js
+++ static/landing.js
@@ -1,16 +1,17 @@
-gsap.to("#cards #bstr", {
-  x: "80vw",
-  rotate: 360,
-  duration: 3,
-  scrollTrigger: {
-    trigger: "#cards",
-    scroller: "body",
-    start: "top 0%",
-    end: "top -230%",
-    scrub: 5,
-    pin: true,
-  },
-});
+// gsap.to("#cards #bstr", {
+//   x: "80vw",
+//   rotate: 360,
+//   duration: 3,
+//   scrollTrigger: {
+//     trigger: "#cards",
+//     scroller: "body",
+//     start: "top 0%",
+//     end: "top -230%",
+//     scrub: 5,
+//     pin: true,
+//   },
+// });
+
 //xxxxxxxxxxxxxxxxxxxx GSAP END FOR THE CARDS SECTION xxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 const get_started = document.querySelectorAll('.getstart');
